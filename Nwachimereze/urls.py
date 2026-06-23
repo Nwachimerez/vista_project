367c89d1b519eb04ae0335d3a4dcea826d9a713f
Nwachimerez <uchennabenjamine725@gmail.com>
2026-06-23 10:36:26 +0100

    updated file

--- Nwachimereze/urls.py
+++ Nwachimereze/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('blog/', include ("blog.urls")),
 ]
 
-if not settings. DEBUG:
+if  settings. DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
